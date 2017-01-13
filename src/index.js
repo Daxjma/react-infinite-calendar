c8fbdebfaf33b3e8dd0a732f77dc5c3ead11b8e4
Murali Suresh <murali.suresh@fujixerox.co.jp>
2017-01-13 19:38:16 +0900

    Removed default selected date

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ export default class InfiniteCalendar extends Component {
 		layout: 'portrait',
 		display: 'days',
 		multiDate: false,
-		selectedDates: [new Date()],
+		selectedDates: [],
 		min: {year: 1980, month: 0, day: 0},
 		minDate: {year: 1980, month: 0, day: 0},
 		max: {year: 2050, month: 11, day: 31},
